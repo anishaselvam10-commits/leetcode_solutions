6d16e7f72c3736480e2d776fbe3bb45dcafb90cc
anishaselvam10-commits <anishaselvam10@gmail.com>
2026-05-17 18:14:06 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.py
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -1,8 +1,8 @@
-class Solution:
-    def climbStairs(self, n: int, memo = {}) -> int:
-        if n == 0 or n == 1: 
-            return 1
-        if n in memo: 
-            return memo[n]
-        memo[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
-        return memo[n]
+1class Solution:
+2    def climbStairs(self, n: int, memo = {}) -> int:
+3        if n == 0 or n == 1: 
+4            return 1
+5        if n in memo: 
+6            return memo[n]
+7        memo[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
+8        return memo[n]
